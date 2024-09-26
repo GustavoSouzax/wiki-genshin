dd16c6a2ec4792bf9e637b3b1e652878b6bb03d8
Gustavo <93099931+GustavoSouzax@users.noreply.github.com>
2024-09-26 12:46:46 -0500

    Nova adição ao site
    Foi adicionado o perosonagens.html e um menu responsivo dos links sociais (Somente em personagens.hrml) Atualização de alguns estilos...
    
    Update personagens.html

--- scripts/script.js
+++ scripts/script.js
@@ -12,13 +12,13 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-const hamburger = document.querySelector(".hamburger-menu");
+const mMenu = document.querySelector(".mobile-menu");
 const navMenu = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", mobileMenu);
+mMenu.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
+    mMenu.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
 
@@ -27,7 +27,7 @@ const navLinks = document.querySelectorAll(".nav-links a");
 navLinks.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
+    mMenu.classList.remove("active");
     navMenu.classList.remove("active");
 }
 
